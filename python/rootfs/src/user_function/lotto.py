59a0e414c96c2517e714b3a3cdefcbd2e92d4cbe
bossoq <boss.zz@me.com>
2021-06-24 00:34:34 +0700

    [Update] Change random logic to prevent user spamming buy only 1 number

--- python/rootfs/src/user_function/lotto.py
+++ python/rootfs/src/user_function/lotto.py
@@ -4,8 +4,13 @@ import random
 def get_winning_number(lotto_list):
     lotto_number = [player[1] for player in lotto_list]
     unique_lotto_list = list(set(lotto_number))
-    for i in range(5):
-        winning_number = random.choice(unique_lotto_list)
+    if unique_lotto_list > 1:
+        for i in range(5):
+            winning_number = random.choice(unique_lotto_list)
+    else:
+        winning_number = unique_lotto_list[0]
+        while winning_number == unique_lotto_list[0]:
+            winning_number = random.randint(0, 99)
     return int(winning_number)
 
 
@@ -13,7 +18,10 @@ def check_winner(lotto_list):
     win_number = get_winning_number(lotto_list)
     total_income = len(lotto_list) * 5
     tax = 0.2
-    min_prize = 10
+    if len(lotto_list) > 5:
+        min_prize = 10
+    else:
+        min_prize = 5
     win_prize = (total_income * (1 - tax) + (random.random() * 5))
     winning_dict = {}
     for lotto in lotto_list:
