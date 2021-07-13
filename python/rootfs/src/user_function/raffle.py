ae992ebed38b786cbadb3117ba196f84e21b6b48
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2021-07-13 11:24:53 +0700

    [Add] add raffle to raffle branch
    (not finish yet)

---
+++ python/rootfs/src/user_function/raffle.py
@@ -0,0 +1,13 @@
+import random
+
+rafflelist = []
+
+# beginraffle = "พิม !raffle เพื่อเข้ามาลุ้นของรางวัลกาชาจากสนิฟเลย!"
+
+def rafflesave():
+    rafflelist.append()
+
+def run_raffle():
+    winner = random.choice(rafflelist)
+    rafllewinner = winner + "คือผู้โชคดีค่า!"
+    
