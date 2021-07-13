d71f9869110f997ff5e49cd7e3e5e6244bebd618
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2021-07-13 13:37:45 +0700

    [update] raffle

--- python/rootfs/src/user_function/raffle.py
+++ python/rootfs/src/user_function/raffle.py
@@ -7,7 +7,14 @@ rafflelist = []
 def rafflesave():
     rafflelist.append()
 
-def run_raffle():
-    winner = random.choice(rafflelist)
-    rafllewinner = winner + "คือผู้โชคดีค่า!"
-    
+def get_winner(lotto_list):
+    rafflelist = [player[1] for player in rafflesave]
+    unique_raffle = list(set(rafflelist))
+    if len(unique_raffle) > 1:
+        for i in range(5):
+            winning_list = random.choice(unique_raffle)
+    else:
+        winning_list = unique_raffle[0]
+        while winning_list == unique_raffle[0]:
+            winning_list = random.randint(10000)
+    return int(winning_list)
