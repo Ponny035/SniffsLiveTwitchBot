534675a37bca9e0af33fab8ceb56af5df66c560e
bossoq <boss.zz@me.com>
2021-07-16 22:02:38 +0700

    [Refactor] Refactoring code in Oak API Server / Preparing for webfeed

--- api/rootfs/app/routes/wsServer.ts
+++ api/rootfs/app/routes/wsServer.ts
@@ -1,9 +1,11 @@
 import { WebSocket, isWebSocketCloseEvent } from "https://deno.land/std@0.99.0/ws/mod.ts";
-import { preparedResponse } from "./songselector.ts"
+import { getTimestamp } from "../functions/gettimestamp.ts"
+import { firstHandShake } from "../controllers/songselector.ts"
 
 let connections = new Array;
 
-export const handleSocket = async (ws: WebSocket): Promise<void> =>{
+export const handleSocket = async (ctx: any): Promise<void> =>{
+    const ws: WebSocket = await ctx.upgrade()
     console.log(`[_API] [${getTimestamp()}] Websocket connection established`)
     connections.push(ws)
     for await (const event of ws){
@@ -11,21 +13,15 @@ export const handleSocket = async (ws: WebSocket): Promise<void> =>{
             connections = connections.filter(websocket => websocket !== ws)
             console.log(`[_API] [${getTimestamp()}] Websocket connection closed`)
         }else{
-            preparedResponse()
+            firstHandShake()
         }
     }
 }
 
-export const broadcastEvents = (event: string) =>{
+export const socketSongList = (event: string) =>{
     for(const websocket of connections){
         if(!websocket.isClosed){
             websocket.send(event)
         }
     }
-}
-
-const getTimestamp = (): string =>{
-    const now = new Date()
-    const timeString = now.toISOString().slice(0, 10)+" "+now.toISOString().slice(11, 19)
-    return timeString
 }
--- api/rootfs/app/server.ts
+++ api/rootfs/app/server.ts
@@ -1,25 +1,19 @@
-import { Application, Router, send } from "https://deno.land/x/oak@v7.5.0/mod.ts"
-import { queryList,
-         songVote,
-         selectSong,
-         deleteSong,
-         clearList,
-         removeNowPlaying } from "./songselector.ts"
-import { handleSocket } from "./wsServer.ts"
+import { Application, send } from "https://deno.land/x/oak@v7.5.0/mod.ts"
+import { getTimestamp } from "./functions/gettimestamp.ts"
+import songselectorRouter from "./routes/songselector.ts"
 
-const port = 8000
+const port: number = 8000
 const app = new Application()
-const router = new Router()
 
 // Logger
-app.use(async (ctx, next) =>{
+app.use(async(ctx: any, next) =>{
     await next()
     const rt = ctx.response.headers.get("X-Response-Time")
     console.log(`[_API] [${getTimestamp()}] ${ctx.request.method} ${ctx.request.url} - ${rt}`)
 })
 
 // Timing
-app.use(async (ctx, next) =>{
+app.use(async(ctx: any, next) =>{
     const start = Date.now();
     await next()
     const ms = Date.now() - start
@@ -27,135 +21,22 @@ app.use(async (ctx, next) =>{
     ctx.response.headers.append("Access-Control-Allow-Origin", "*")
 })
 
-app.use(router.routes())
-app.use(router.allowedMethods())
-
-// declare type of Song
-interface Song {
-    songKey: string,
-    songName: string,
-    vote: number,
-    ts: Date
-}
-
-// declare type of song request
-interface Req {
-    songKey: string
-}
-
-// declare type of list clear request
-interface Confirm {
-    confirm: boolean
-}
+app.use(songselectorRouter.routes())
+app.use(songselectorRouter.allowedMethods())
 
 // serve static
-app.use(async(ctx) =>{
+app.use(async(ctx: any, next) =>{
+    await next()
     await send(ctx, ctx.request.url.pathname, {
         root: `${Deno.cwd()}/static`,
         index: "index.html",
     })
 })
 
-// @desc GET handle WS
-router.get("/ws", async (ctx: any) =>{
-    const sock = await ctx.upgrade();
-    handleSocket(sock);
+app.addEventListener("listen", ({ secure, hostname, port }) =>{
+    const protocol = secure ? "https://" : "http://"
+    const url = `${protocol}${hostname ?? "localhost"}:${port}`
+    console.log(`[_API] [${getTimestamp()}] Listening on : ${url}`)
 })
-
-// @desc GET all sorted song list
-router.get("/api/v1/songlist", ({response}: {response: any}) =>{
-    const res = queryList()
-    response.status = res.status
-    response.body = res.response
-})
-
-// @desc POST insert song / vote to song list
-router.post("/api/v1/vote", async({request, response}: {request: any, response: any}) =>{
-    if(request.hasBody){
-        const body = request.body()
-        const song: Song = await body.value
-        response.body = songVote(song)
-    }else{
-        response.status = 404
-        response.body = {
-            success: false,
-            msg: "no data input"
-        }
-    }  
-})
-
-// @desc POST select song
-router.post("/api/v1/select", async({request, response}: {request: any, response: any}) =>{
-    if(request.hasBody){
-        const body = request.body()
-        const req: Req = await body.value
-        const res = selectSong(req)
-        response.status = res.status
-        response.body = res.response
-    }else{
-        response.status = 404
-        response.body = {
-            success: false,
-            msg: "no data input"
-        }
-    }
-})
-
-// @desc POST delete song by songname
-router.post("/api/v1/del", async({request, response}: {request: any, response: any}) =>{
-    if(request.hasBody){
-        const body = request.body()
-        const req: Req = await body.value
-        const res = deleteSong(req)
-        response.status = res.status
-        response.body = res.response
-    }else{
-        response.status = 404
-        response.body = {
-            success: false,
-            msg: "no data input"
-        }
-    }
-})
-
-// @desc POST clear song list
-router.post("/api/v1/clear", async({request, response}: {request: any, response: any}) =>{
-    if(request.hasBody){
-        const body = request.body()
-        const req: Confirm = await body.value
-        const res = clearList(req)
-        response.status = res.status
-        response.body = res.response
-    }else{
-        response.status = 404
-        response.body = {
-            success: false,
-            msg: "no data input"
-        }
-    }
-})
-
-// @desc POST rem now playing
-router.post("/api/v1/rem", async({request, response}: {request: any, response: any}) =>{
-    if(request.hasBody){
-        const body = request.body()
-        const req: Confirm = await body.value
-        const res = removeNowPlaying(req)
-        response.status = res.status
-        response.body = res.response
-    }else{
-        response.status = 404
-        response.body = {
-            success: false,
-            msg: "no data input"
-        }
-    }
-})
-
-const getTimestamp = (): string =>{
-    const now = new Date()
-    const timeString = now.toISOString().slice(0, 10)+" "+now.toISOString().slice(11, 19)
-    return timeString
-}
 
 await app.listen({ port: port })
