6729a325095c849e9ed1c92a91fbd31255aafd42
bossoq <boss.zz@me.com>
2021-07-18 17:55:15 +0700

    [Style] Change JS/CSS style in front end function

--- api/rootfs/app/static/songfeed.js
+++ api/rootfs/app/static/songfeed.js
@@ -10,10 +10,10 @@ const containerClass = "columns is-mobile is-size-4 text-bolder mb-3";
 const listClass = "column has-text-centered p-0";
 const nameClass = "column is-three-fifths p-0 nowrap-text";
 const scoreClass = "column is-one-quarter has-text-centered p-0";
-const playButton = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pg0gPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE2LjAuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgLS0+DSA8IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPg0gPHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJDYXBhXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0gd2lkdGg9IjQwOC4yMjFweCIgaGVpZ2h0PSI0MDguMjIxcHgiIHZpZXdCb3g9IjAgMCA0MDguMjIxIDQwOC4yMjEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDQwOC4yMjEgNDA4LjIyMTsiDSB4bWw6c3BhY2U9InByZXNlcnZlIj4NIDxnPg0gPGc+DSA8cGF0aCBmaWxsPSIjRkZGRkZGIiBkPSJNMjA0LjExLDBDOTEuMzg4LDAsMCw5MS4zODgsMCwyMDQuMTExYzAsMTEyLjcyNSw5MS4zODgsMjA0LjExLDIwNC4xMSwyMDQuMTFjMTEyLjcyOSwwLDIwNC4xMS05MS4zODUsMjA0LjExLTIwNC4xMQ0gQzQwOC4yMjEsOTEuMzg4LDMxNi44MzksMCwyMDQuMTEsMHogTTI4Ni41NDcsMjI5Ljk3MWwtMTI2LjM2OCw3Mi40NzFjLTE3LjAwMyw5Ljc1LTMwLjc4MSwxLjc2My0zMC43ODEtMTcuODM0VjE0MC4wMTINIGMwLTE5LjYwMiwxMy43NzctMjcuNTc1LDMwLjc4MS0xNy44MjdsMTI2LjM2OCw3Mi40NjZDMzAzLjU1MSwyMDQuNDAzLDMwMy41NTEsMjIwLjIxNywyODYuNTQ3LDIyOS45NzF6Ii8+DSA8L2c+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8L3N2Zz4N";
+const playButton = "fas fa-play-circle";
 
 // generate song list element
-function generateSongList(songList, index){
+const generateSongList = (songList, index) =>{
     if(songList[index - 1] === undefined){
         songName = "No Song";
         vote = 0;
@@ -24,25 +24,25 @@ function generateSongList(songList, index){
         modClass = "";
     }
 
-    var containerElement = document.createElement("div");
+    const containerElement = document.createElement("div");
     containerElement.className = containerClass + modClass;
     containerElement.id = `container${index}`;
 
-    var listElement = document.createElement("div");
+    const listElement = document.createElement("div");
     listElement.className = listClass;
     listElement.innerHTML = `[${index.toString()}]`;
 
-    var nameElement = document.createElement("div");
+    const nameElement = document.createElement("div");
     nameElement.className = nameClass;
     nameElement.innerHTML = songName;
     nameElement.id = `name${index}`;
 
-    var voteElement = document.createElement("div");
+    const voteElement = document.createElement("div");
     voteElement.className = scoreClass;
     voteElement.innerHTML = `${vote.toString()} pts`;
     voteElement.id = `vote${index}`;
 
-    var songListElement = document.getElementById("list-title");
+    const songListElement = document.getElementById("list-title");
     songListElement.appendChild(containerElement);
     containerElement.appendChild(listElement);
     containerElement.appendChild(nameElement);
@@ -54,23 +54,23 @@ function generateSongList(songList, index){
         }else{
             modClass = " is-hidden";
         }
-        var noElement = document.createElement("div");
+        const noElement = document.createElement("div");
         noElement.className = containerClass + modClass;
         noElement.classList.remove("mb-3");
         noElement.id = "emptylist";
 
-        var nosongElement = document.createElement("div");
+        const nosongElement = document.createElement("div");
         nosongElement.className = "column has-text-centered p-0";
         nosongElement.innerHTML = "ขอเพลง !sr กันเข้ามา";
 
         songListElement.appendChild(noElement);
         noElement.appendChild(nosongElement);
     }
-}
+};
 
 // generate now playing element
-function generateNowPlaying(song){
-    var nowPlayingListElement = document.getElementById("nowplaying-title");
+const generateNowPlaying = (song) =>{
+    const nowPlayingListElement = document.getElementById("nowplaying-title");
     if(song === "nothing" || song === undefined){
         modnoClass = "";
         modSongClass = " is-hidden";
@@ -82,25 +82,25 @@ function generateNowPlaying(song){
         songName = song.songName;
         vote = song.vote;
     }
-    var containerElement = document.createElement("div");
+    const containerElement = document.createElement("div");
     containerElement.className = containerClass + modSongClass;
     containerElement.id = "nowplaying";
 
-    var imgElement = document.createElement("div");
+    const imgElement = document.createElement("div");
     imgElement.className = listClass;
-    var svgElement = document.createElement("img");
-    svgElement.setAttribute("width", "36");
-    svgElement.setAttribute("height", "36");
-    svgElement.setAttribute("stroke", "white");
-    svgElement.src = `data:image/svg+xml;base64,${playButton}`;
-    imgElement.appendChild(svgElement);
+    const spanElement = document.createElement("span");
+    spanElement.className = "icon";
+    const playElement = document.createElement("i");
+    playElement.className = playButton;
+    spanElement.appendChild(playElement);
+    imgElement.appendChild(spanElement);
 
-    var nameElement = document.createElement("div");
+    const nameElement = document.createElement("div");
     nameElement.className = nameClass;
     nameElement.innerHTML = songName;
     nameElement.id = "nowplayingName";
 
-    var voteElement = document.createElement("div");
+    const voteElement = document.createElement("div");
     voteElement.className = scoreClass;
     voteElement.innerHTML = `${vote} pts`;
     voteElement.id = "nowplayingVote";
@@ -110,25 +110,25 @@ function generateNowPlaying(song){
     containerElement.appendChild(nameElement);
     containerElement.appendChild(voteElement);
 
-    var noElement = document.createElement("div");
+    const noElement = document.createElement("div");
     noElement.className = containerClass + modnoClass;
     noElement.id = "noplaying";
 
-    var nosongElement = document.createElement("div");
+    const nosongElement = document.createElement("div");
     nosongElement.className = "column has-text-centered p-0";
     nosongElement.innerHTML = "ยังไม่มีเพลงที่เล่นน้า";
 
     nowPlayingListElement.appendChild(noElement);
     noElement.appendChild(nosongElement);
-}
+};
 
 // refresh song list
-function refreshSongList(songList){
+const refreshSongList = (songList) =>{
     for(i = 0; i < 5; i++){
         index = i + 1;
-        var containerElement = document.getElementById(`container${index.toString()}`);
-        var nameElement = document.getElementById(`name${index.toString()}`);
-        var voteElement = document.getElementById(`vote${index.toString()}`);
+        const containerElement = document.getElementById(`container${index.toString()}`);
+        const nameElement = document.getElementById(`name${index.toString()}`);
+        const voteElement = document.getElementById(`vote${index.toString()}`);
         if(songList[i] !== undefined){
             nameElement.innerHTML = songList[i].songName;
             voteElement.innerHTML = `${songList[i].vote.toString()} pts`;
@@ -146,7 +146,7 @@ function refreshSongList(songList){
             wrapContentInMarquee(nameElement);
         }
     }
-    var noElement = document.getElementById("emptylist");
+    const noElement = document.getElementById("emptylist");
     if(songList.length === 0){
         if(noElement.classList.contains("is-hidden")){
             noElement.classList.remove("is-hidden");
@@ -156,14 +156,14 @@ function refreshSongList(songList){
             noElement.classList.add("is-hidden");
         }
     }
-}
+};
 
 // refresh nowplaying
-function refreshNowPlaying(song){
-    var containerElement = document.getElementById("nowplaying");
-    var nameElement = document.getElementById("nowplayingName");
-    var voteElement = document.getElementById("nowplayingVote");
-    var noElement = document.getElementById("noplaying");
+const refreshNowPlaying = (song) =>{
+    const containerElement = document.getElementById("nowplaying");
+    const nameElement = document.getElementById("nowplayingName");
+    const voteElement = document.getElementById("nowplayingVote");
+    const noElement = document.getElementById("noplaying");
     if(song !== undefined){
         nameElement.innerHTML = song.songName;
         voteElement.innerHTML = song.vote.toString()+" pts";
@@ -186,7 +186,7 @@ function refreshNowPlaying(song){
     if(isElementOverflowing(nameElement)){
         wrapContentInMarquee(nameElement);
     }
-}
+};
 
 const ws = new WebSocket(websocketUrl);
 ws.onopen = function(){
@@ -205,17 +205,17 @@ for (i = 0; i < 5; i++){
 }
 generateNowPlaying(nowPlaying);
 
-function isElementOverflowing(element){
-    var overflowX = element.offsetWidth < element.scrollWidth;
+const isElementOverflowing = (element) =>{
+    const overflowX = element.offsetWidth < element.scrollWidth;
     return(overflowX);
-}
+};
 
-function wrapContentInMarquee(element){
-    var marquee = document.createElement('div'),
+const wrapContentInMarquee = (element) =>{
+    const marquee = document.createElement('div'),
     contents = element.innerText;
 
     marquee.innerText = contents;
     marquee.className = 'sliding-text';
     element.innerHTML = '';
     element.appendChild(marquee);
-}
+};
--- api/rootfs/app/static/songlist.js
+++ api/rootfs/app/static/songlist.js
@@ -14,45 +14,45 @@ const listClass = "column has-text-centered p-0";
 const nameClass = "column is-three-fifths p-0 nowrap-text";
 const scoreClass = "column is-one-fifths has-text-centered p-0";
 const linkClass = "column is-one-fifths has-text-centered p-0";
-const playButton = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pg0gPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE2LjAuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgLS0+DSA8IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPg0gPHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJDYXBhXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0gd2lkdGg9IjQwOC4yMjFweCIgaGVpZ2h0PSI0MDguMjIxcHgiIHZpZXdCb3g9IjAgMCA0MDguMjIxIDQwOC4yMjEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDQwOC4yMjEgNDA4LjIyMTsiDSB4bWw6c3BhY2U9InByZXNlcnZlIj4NIDxnPg0gPGc+DSA8cGF0aCBkPSJNMjA0LjExLDBDOTEuMzg4LDAsMCw5MS4zODgsMCwyMDQuMTExYzAsMTEyLjcyNSw5MS4zODgsMjA0LjExLDIwNC4xMSwyMDQuMTFjMTEyLjcyOSwwLDIwNC4xMS05MS4zODUsMjA0LjExLTIwNC4xMQ0gQzQwOC4yMjEsOTEuMzg4LDMxNi44MzksMCwyMDQuMTEsMHogTTI4Ni41NDcsMjI5Ljk3MWwtMTI2LjM2OCw3Mi40NzFjLTE3LjAwMyw5Ljc1LTMwLjc4MSwxLjc2My0zMC43ODEtMTcuODM0VjE0MC4wMTINIGMwLTE5LjYwMiwxMy43NzctMjcuNTc1LDMwLjc4MS0xNy44MjdsMTI2LjM2OCw3Mi40NjZDMzAzLjU1MSwyMDQuNDAzLDMwMy41NTEsMjIwLjIxNywyODYuNTQ3LDIyOS45NzF6Ii8+DSA8L2c+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8Zz4NIDwvZz4NIDxnPg0gPC9nPg0gPGc+DSA8L2c+DSA8L3N2Zz4N";
+const playButton = "fas fa-play-circle";
 
 // generate song list element
 const generateSongList = (songList) =>{
-    var songListElement = document.getElementById("list-title");
+    const songListElement = document.getElementById("list-title");
 
     for(i = 0; i < oldLenSongList; i++){
         index = i + 1;
-        var containerElement = document.getElementById(`container${index}`);
+        const containerElement = document.getElementById(`container${index}`);
         containerElement.parentNode.removeChild(containerElement);
     }
 
     if(lenSongList > 0){
         let index = 1;
-        songList.forEach(songData => {
-            var containerElement = document.createElement("div");
+        songList.forEach(songData =>{
+            const containerElement = document.createElement("div");
             containerElement.className = containerClass;
             containerElement.id = `container${index}`;
 
-            var listElement = document.createElement("div");
+            const listElement = document.createElement("div");
             listElement.className = listClass;
             listElement.innerHTML = `[${index.toString()}]`;
 
-            var nameElement = document.createElement("div");
+            const nameElement = document.createElement("div");
             nameElement.className = nameClass;
             nameElement.innerHTML = songData.songName;
             nameElement.id = `name${index}`;
 
-            var voteElement = document.createElement("div");
+            const voteElement = document.createElement("div");
             voteElement.className = scoreClass;
             voteElement.innerHTML = `${songData.vote.toString()} pts`;
             voteElement.id = `vote${index}`;
 
-            var selElement = document.createElement("div");
+            const selElement = document.createElement("div");
             selElement.className = linkClass;
             selElement.innerHTML = `<a href="javascript:;" onclick="selectSong(${index})">Select</a>`;
             selElement.id = `sel${index}`;
 
-            var delElement = document.createElement("div");
+            const delElement = document.createElement("div");
             delElement.className = linkClass;
             delElement.innerHTML = `<a href="javascript:;" onclick="deleteSong(${index})">Delete</a>`;
             delElement.id = `del${index}`;
@@ -69,14 +69,14 @@ const generateSongList = (songList) =>{
 };
 
 const generateSongListEmpty = (delEmpty) =>{
-    var songListElement = document.getElementById("list-title");
-    var noElement = document.getElementById("emptylist");
+    const songListElement = document.getElementById("list-title");
+    let noElement = document.getElementById("emptylist");
     if(noElement === null && !delEmpty){
         noElement = document.createElement("div");
         noElement.className = containerClass;
         noElement.id = "emptylist";
 
-        var nosongElement = document.createElement("div");
+        const nosongElement = document.createElement("div");
         nosongElement.className = "column has-text-centered p-0";
         nosongElement.innerHTML = "ขอเพลง !sr กันเข้ามา";
 
@@ -89,8 +89,8 @@ const generateSongListEmpty = (delEmpty) =>{
 
 // generate now playing element
 const generateNowPlaying = (song) =>{
-    var nowPlayingListElement = document.getElementById("nowplaying-title");
-    var containerElement = document.getElementById("nowplaying");
+    const nowPlayingListElement = document.getElementById("nowplaying-title");
+    let containerElement = document.getElementById("nowplaying");
 
     if(containerElement !== null){
         containerElement.parentNode.removeChild(containerElement);
@@ -101,26 +101,26 @@ const generateNowPlaying = (song) =>{
         containerElement.className = containerClass;
         containerElement.id = "nowplaying";
 
-        var imgElement = document.createElement("div");
+        const imgElement = document.createElement("div");
         imgElement.className = listClass;
-        var svgElement = document.createElement("img");
-        svgElement.setAttribute("width", "36");
-        svgElement.setAttribute("height", "36");
-        svgElement.setAttribute("stroke", "white");
-        svgElement.src = `data:image/svg+xml;base64,${playButton}`;
-        imgElement.appendChild(svgElement);
+        const spanElement = document.createElement("span");
+        spanElement.className = "icon";
+        const playElement = document.createElement("i");
+        playElement.className = playButton;
+        spanElement.appendChild(playElement);
+        imgElement.appendChild(spanElement);
 
-        var nameElement = document.createElement("div");
+        const nameElement = document.createElement("div");
         nameElement.className = nameClass;
         nameElement.innerHTML = song.songName;
         nameElement.id = "nowplayingName";
 
-        var voteElement = document.createElement("div");
+        const voteElement = document.createElement("div");
         voteElement.className = scoreClass;
         voteElement.innerHTML = `${song.vote} pts`;
         voteElement.id = "nowplayingVote";
 
-        var delElement = document.createElement("div");
+        const delElement = document.createElement("div");
         delElement.className = linkClass;
         delElement.innerHTML = "<a href='javascript:;' onclick=deleteNowPlaying()>Delete</a>";
         delElement.id = "delNowPlaying";
@@ -134,14 +134,14 @@ const generateNowPlaying = (song) =>{
 };
 
 const generateNowPlayingEmpty = (delEmpty) =>{
-    var nowPlayingListElement = document.getElementById("nowplaying-title");
-    var noElement = document.getElementById("noplaying");
+    const nowPlayingListElement = document.getElementById("nowplaying-title");
+    let noElement = document.getElementById("noplaying");
     if(noElement === null && !delEmpty){
         noElement = document.createElement("div");
         noElement.className = containerClass;
         noElement.id = "noplaying";
     
-        var nosongElement = document.createElement("div");
+        const nosongElement = document.createElement("div");
         nosongElement.className = "column has-text-centered p-0";
         nosongElement.innerHTML = "ยังไม่มีเพลงที่เล่นน้า";
     
@@ -157,7 +157,7 @@ const selectSong = (songIndex) =>{
     const songKey = songList[Number(songIndex) - 1].songKey;
     const url = `http://${window.location.hostname}:8000/api/v1/select`;
 
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open("POST", url);
 
     xhr.setRequestHeader("Accept", "application/json");
@@ -172,7 +172,7 @@ const deleteSong = (songIndex) =>{
     const songKey = songList[Number(songIndex) - 1].songKey;
     const url = `http://${window.location.hostname}:8000/api/v1/del`;
 
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open("POST", url);
 
     xhr.setRequestHeader("Accept", "application/json");
@@ -186,7 +186,7 @@ const deleteSong = (songIndex) =>{
 const deleteNowPlaying = () =>{
     const url = `http://${window.location.hostname}:8000/api/v1/rem`;
 
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open("POST", url);
 
     xhr.setRequestHeader("Accept", "application/json");
@@ -199,7 +199,7 @@ const deleteNowPlaying = () =>{
 
 const ws = new WebSocket(websocketUrl);
 ws.onopen = () =>{
-    ws.send("Handshake");
+    ws.send("songfeedHandshake");
 };
 ws.addEventListener("message", (event) =>{
     response = (JSON.parse(event.data));
@@ -226,12 +226,12 @@ ws.addEventListener("message", (event) =>{
 });
 
 const isElementOverflowing = (element) =>{
-    var overflowX = element.offsetWidth < element.scrollWidth;
+    const overflowX = element.offsetWidth < element.scrollWidth;
     return(overflowX);
 };
 
 const wrapContentInMarquee = (element) =>{
-    var marquee = document.createElement('div'),
+    const marquee = document.createElement('div'),
     contents = element.innerText;
 
     marquee.innerText = contents;
