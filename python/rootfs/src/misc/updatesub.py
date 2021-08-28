67aefd7527116655047e8f8cd62f9f8c79002d36
Kittipos Wajakajornrit <boss.zz@me.com>
2021-08-28 23:09:34 +0700

    Cleanup updatesub.py & Add typehint & Change update logic

--- python/rootfs/src/misc/updatesub.py
+++ python/rootfs/src/misc/updatesub.py
@@ -4,7 +4,7 @@ from src.db_function import check_exist, insert, retrieve, update
 from src.timefn.timestamp import get_timestamp
 
 
-async def update_submonth(username, rawdata):
+async def update_submonth(username: str, rawdata: str):
     submonth = 0
     try:
         submonth = int(re.search("(?<=subscriber/)([0-9]+)", rawdata)[0])
@@ -13,9 +13,10 @@ async def update_submonth(username, rawdata):
     if submonth > 0:
         if check_exist(username):
             userdata = retrieve(username)
-            userdata["submonth"] = submonth
-            update(userdata)
-            print(f"[INFO] [{get_timestamp()}] Update {username} submonth to {submonth} months")
+            if userdata["submonth"] != submonth:
+                userdata["submonth"] = submonth
+                update(userdata)
+                print(f"[INFO] [{get_timestamp()}] Update {username} submonth to {submonth} months")
         else:
             userdata = {
                 "username": username,
