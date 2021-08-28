089ec3e6eb8c84962ee6e3f013558ae7c1f4b381
Kittipos Wajakajornrit <boss.zz@me.com>
2021-08-28 22:25:02 +0700

    Cleanup automod.py & Add typehint

--- python/rootfs/src/misc/automod.py
+++ python/rootfs/src/misc/automod.py
@@ -1,125 +1,6 @@
-# from typing import Dict, Optional, Union
-# from dataclasses import dataclass
-# from contextlib import contextmanager
-# from collections import defaultdict  # second choice of try
-# from . import db
-# import warnings
-from datetime import datetime
 import re
-# import logging
 
-# log = logging.getLogger(__name__)
-
-
-# @dataclass
-# class Untimeout:
-#     pass
-
-
-# @dataclass
-# class Unban:
-#     pass
-
-
-# @dataclass
-# class Timeout:
-#     duration: int
-#     reason: Optional[str]
-#     once: bool
-
-# @dataclass
-# class Ban:
-#     reason: Optional[str]
-
-# ModerationAction = Union[Untimeout, Unban, Timeout, Ban]
-
-# def _combine_resons(a: Optional[str], b:Optional[str]) -> Optional[str]:
-#     if a is None and b is None:
-#         return None
-
-#     if a is None:
-#         return b
-
-#     if b is None:
-#         return a
-
-#     return f"{a} + {b}"
-
-
-# class ModerationActions:
-#     # login -> action excute
-#     actions: Dict[str, ModerationAction]
-
-#     def __init__(self) -> None:
-#         super().__init__()
-#         self.actions = {}
-
-#     def add(self, login: str, action: ModerationAction) -> None:
-#         if login not in self.actions:
-#             self.actions[login] = action
-#             return
-
-#         existing_action = self.actions[login]
-
-#         if isinstance(action, Ban):
-#             # combine the two
-#             self.actions[login] = Ban(reason=_combine_resons(existing_action.reason, action.reason))
-#         else:
-#             # ban lower tier action
-#             self.actions[login] = action
-#         return
-
-#         if isinstance(action, Timeout):
-#             if isinstance(existing_action, Ban):
-#                 # Existing action is higher-tier
-#                 return
-
-#             if isinstance(existing_action,Timeout):
-#                 # combine two
-#                 self.actions[login] = Timeout(
-#                     duration=max(action.duration, existing_action.duration),
-#                     reason=_combine_resons(existing_action.reason, action.reason),
-#                     once=existing_action.once and action.once,
-#                 )
-#             else:
-#                 # timeout lower tier action
-#                 self.actions[login] = action
-#             return
-
-#             if isinstance(action, Unban):
-#                 if isinstance(existing_action, Ban) or isinstance(existing_action, Timeout):
-#                     # exist action is higher tier
-#                     return
-
-#                 if isinstance(existing_action, Unban):
-#                     pass
-#                 return
-
-#     def execute(self, bot) -> None:
-#         for login, action in self.actions.items():
-#             if isinstance(action, Ban):
-#                 bot.ban_login(login, action.reason)
-#             if isinstance(action, Timeout):
-#                 bot.timeout_login(login, action.duration, action.reason, action.once)
-#             if isinstance(action, Unban):
-#                 bot.unban_login(login)
-#             if isinstance(action, Untimeout):
-#                 bot.untimeout_login(login)
-
-
-# @contextmanager
-# def new_message_processing_scope(bot):
-#     bot.thread_locals.moderation_actions = ModerationActions()
-
-#     try:
-#         yield
-#     finally:
-#         mod_actions = bot.thread_locals.moderation_actions
-#         bot.thread_locals.moderation_actions = None
-#         try:
-#             mod_actions.execute(bot)
-#         except:
-#             log.exception("Faild to execute moderation action after messsage process scope end")
+from src.timefn.timestamp import get_timestamp
 
 
 class automod:
@@ -127,22 +8,19 @@ class automod:
         # self.CURSES = ("fuck", "poo", "boo", "you are suck")
         self.warning_users = {}
 
-    def get_timestamp(self):
-        return datetime.utcnow().replace(microsecond=0)
-
-    async def auto_mod(self, user, role, message, raw_data, send_message, timeout, ban):
+    async def auto_mod(self, user: str, role: bool, message: str, raw_data: str, send_message, timeout, ban):
         msg_id = re.search(r"(?<=id=)(.*?)(?=;)", raw_data)[0]
-        await self.auto_cursesword(user, role, message, msg_id, send_message, timeout, ban)
+        # await self.auto_cursesword(user, role, message, msg_id, send_message, timeout, ban)
         await self.auto_removelink(user, role, message, msg_id, send_message, timeout, ban)
         await self.auto_removetwitchlink(user, message, msg_id, send_message, timeout, ban)
 
-    async def auto_cursesword(self, user, role, message, msg_id, send_message, timeout, ban):
-        # if any([curse in message.lower() for curse in self.CURSES]):
-        #     warning_curses_timers = (1, 5, 60)
-        #     await self.warn(user, send_message, timeout, ban, warning_curses_timers, msg_id, reason="curses")
-        pass
+    # async def auto_cursesword(self, user, role, message, msg_id, send_message, timeout, ban):
+    #     if any([curse in message.lower() for curse in self.CURSES]):
+    #         warning_curses_timers = (1, 5, 60)
+    #         await self.warn(user, send_message, timeout, ban, warning_curses_timers, msg_id, reason="curses")
+    #     pass
 
-    async def auto_removelink(self, user, role, message, msg_id, send_message, timeout, ban):
+    async def auto_removelink(self, user: str, role: bool, message: str, msg_id: str, send_message, timeout, ban):
         # if any([curse in message.lower() for curse in self.CURSES]):
         #     warning_curses_timers = (1, 5, 60)
         #     await self.warn(user, send_message, timeout, ban, warning_curses_timers, reason="curses")
@@ -154,16 +32,14 @@ class automod:
                 warning_link_timers = (0, 0, 1, 5, 10, 30, 60)
                 await self.warn(user, send_message, timeout, ban, warning_link_timers, msg_id, reason="paste_link_by_non_sub")
 
-    async def auto_removetwitchlink(self, user, message, msg_id, send_message, timeout, ban):
+    async def auto_removetwitchlink(self, user: str, message: str, msg_id: str, send_message, timeout, ban):
         check_url = bool(re.match("https?://(www.)?twitch.tv/", message))
         check_clipurl = not bool(re.match("https?://(www.)?twitch.tv/.*/clip/.*", message))
         if check_url and check_clipurl:
             warning_link_timers = (0, 0, 10, 30, 60)
             await self.warn(user, send_message, timeout, ban, warning_link_timers, msg_id, reason="paste_twitch_link")
 
-    async def warn(self, user, send_message, timeout, ban, timers, msg_id=None, reason=None):
-        # Warnings = db("SELECT warning FROM user WHERE user id?",
-        # user["id"])
+    async def warn(self, user: str, send_message, timeout, ban, timers: tuple[int], msg_id=None, reason=None):
         warning = 0
         try:
             warning = self.warning_users[user][reason]
@@ -175,16 +51,12 @@ class automod:
             timeout_mins = timers[warning]
             if timeout_mins == 0:
                 await send_message(f"/delete {msg_id}")
-                print(f"[AMOD] [{self.get_timestamp()}] DeleteMessage: {user} Duration: {timeout_mins} Reason: {reason}")
+                print(f"[AMOD] [{get_timestamp()}] DeleteMessage: {user} Duration: {timeout_mins} Reason: {reason}")
                 await send_message(f"@{user} เตือนก่อนน้า sniffsAngry sniffsAngry sniffsAngry")
             else:
                 await timeout(user, timeout_mins * 60, f"ไม่ได้น้า เตือนครั้งที่ {self.warning_users[user][reason]}")
-                print(f"[AMOD] [{self.get_timestamp()}] Timeout: {user} Duration: {timeout_mins} Reason: {reason}")
+                print(f"[AMOD] [{get_timestamp()}] Timeout: {user} Duration: {timeout_mins} Reason: {reason}")
                 await send_message(f"@{user} ไม่เชื่อฟังสนิฟ sniffsAngry sniffsAngry sniffsAngry ไปนั่งพักก่อนซัก {timeout_mins} นาทีนะ")
-
-            # db.execute("UPDATE users SET warnings = Warnings +1 WHERE UserID = ?",
-            # user["ID"])
-
         else:
             self.warning_users[user][reason] = 0  # reset counter
             await ban(user, f"ละเมิดกฎครบ {self.warning_users[user][reason]} ครั้ง บินไปซะ")
