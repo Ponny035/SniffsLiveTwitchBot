094e75240df2f488b08608be947624ca6524cb59
Kittipos Wajakajornrit <boss.zz@me.com>
2021-08-30 21:29:10 +0700

    Remove Webfeed to another project

--- webfeed/pages/api/songlist.ts
+++
@@ -1,8 +0,0 @@
-import { preparedResponse } from '../../utils/songSelector'
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(await preparedResponse())
-}
-
-export default handler
--- webfeed/utils/checkHeader.ts
+++
@@ -1,6 +0,0 @@
-const wsToken: string = process.env.WS_KEY || ''
-const serverKey: string = 'jSjens73jZks73'
-
-export const checkHeader = (token: string): boolean => {
-  return token === wsToken || token === serverKey
-}
