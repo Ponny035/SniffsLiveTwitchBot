c7f7ad97427f9d7c3c5866c917765ed4c83c3755
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2024-02-11 01:17:35 +0700

    [Hot-Fix] fix lotto repeating the same number

--- python/rootfs/src/user_function/lotto.py
+++ python/rootfs/src/user_function/lotto.py
@@ -3,17 +3,28 @@ import random
 from src.misc import alldata
 
 
-def get_winning_number():
-    lotto_number = [player[1] for player in alldata.player_lotto_list]
+def get_winning_number(lotto_list):
+    lotto_number = [player[1] for player in lotto_list]
     unique_lotto_list = list(set(lotto_number))
+    conseq_number = []
     if len(unique_lotto_list) > 1:
-        for i in range(5):
+        winning_number = random.choice(unique_lotto_list)
+        conseq_number.append(winning_number)
+        count = 1
+        while count < 2 and winning_number in lotto_number:
             winning_number = random.choice(unique_lotto_list)
+            count += 1
     else:
         winning_number = unique_lotto_list[0]
-        while winning_number == unique_lotto_list[0]:
-            winning_number = random.randint(0, 99)
-    return int(winning_number)
+        while winning_number == unique_lotto_list[0] or winning_number in lotto_number:
+            winning_number = random.randrange(1, 99)
+    if conseq_number.count(winning_number) >= 2:
+        #delete winning number from list unique_lotto_list
+        unique_lotto_list.remove(winning_number)
+        winning_number = random.choice(unique_lotto_list)
+        count = 0
+    return winning_number
+
 
 
 def check_winner():
